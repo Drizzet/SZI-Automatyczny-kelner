8814caca7b84ca5512ed65d3ae1af8c1efaf20a0
Kublen <jakub.lenartowicz@gmail.com>
2018-05-06 13:28:21 +0200

    kelner.py fix

--- kelner.py
+++ kelner.py
@@ -4,8 +4,8 @@ pygame.init()
 screen = pygame.display.set_mode((850, 850))
 done = False
 is_blue = True
-kelnerX = 30
-kelnerY = 30
+kelnerX = 25
+kelnerY = 25
 krataX = 0
 krataY = 0
 clock = pygame.time.Clock()
@@ -18,29 +18,29 @@ while not done:
             is_blue = not is_blue
 
     pressed = pygame.key.get_pressed()
-    if pressed[pygame.K_UP]: kelnerY -= 3
-    if pressed[pygame.K_DOWN]: kelnerY += 3
-    if pressed[pygame.K_LEFT]: kelnerX -= 3
-    if pressed[pygame.K_RIGHT]: kelnerX += 3
+    if pressed[pygame.K_UP]: kelnerY -= 50
+    if pressed[pygame.K_DOWN]: kelnerY += 50
+    if pressed[pygame.K_LEFT]: kelnerX -= 50
+    if pressed[pygame.K_RIGHT]: kelnerX += 50
 
     screen.fill((255, 255, 255))
 
     for i in range(0,85):
-        pygame.draw.line(screen,(0,0,0),(krataX+i*10,0),(krataX+i*10,850),1)
-        pygame.draw.line(screen,(0,0,0), (0,krataY+i*10),(850, krataY+i*10), 1)
+        pygame.draw.line(screen,(0,0,0),(krataX+i*50,0),(krataX+i*50,850),1)
+        pygame.draw.line(screen,(0,0,0), (0,krataY+i*50),(850, krataY+i*50), 1)
 
 
 
     pygame.draw.rect(screen, (255, 0, 0) , pygame.Rect(0, 50, 100, 200))
     pygame.draw.rect(screen, (200, 200, 0), pygame.Rect(0, 600, 100, 200))
 
-    pygame.draw.rect(screen, (139, 69, 19), pygame.Rect(350, 100, 100, 100))
+    pygame.draw.rect(screen, (139, 69, 19), pygame.Rect(350, 75, 100, 100))
     pygame.draw.rect(screen, (139, 69, 19), pygame.Rect(350, 370, 100, 100))
-    pygame.draw.rect(screen, (139, 69, 19), pygame.Rect(350, 650, 100, 100))
+    pygame.draw.rect(screen, (139, 69, 19), pygame.Rect(350, 675, 100, 100))
 
-    pygame.draw.rect(screen, (139,69,19), pygame.Rect(600, 100, 100, 100))
+    pygame.draw.rect(screen, (139,69,19), pygame.Rect(600, 75, 100, 100))
     pygame.draw.rect(screen, (139,69,19), pygame.Rect(600, 370, 100, 100))
-    pygame.draw.rect(screen, (139,69,19), pygame.Rect(600, 650, 100, 100))
+    pygame.draw.rect(screen, (139,69,19), pygame.Rect(600, 675, 100, 100))
 
     if is_blue:
         color = (0, 128, 255)
@@ -48,11 +48,11 @@ while not done:
         color = (255, 100, 0)
     pygame.draw.circle(screen, color, (kelnerX, kelnerY),25)
 
-    pygame.draw.circle(screen, (0,102,0), (400, 75), 25)
-    pygame.draw.circle(screen, (0, 102, 0), (400, 225), 25)
+    pygame.draw.circle(screen, (0,102,0), (400, 50), 25)
+    pygame.draw.circle(screen, (0, 102, 0), (400, 200), 25)
 
-    pygame.draw.circle(screen, (0, 102, 0), (650, 75), 25)
-    pygame.draw.circle(screen, (0, 102, 0), (650, 225), 25)
+    pygame.draw.circle(screen, (0, 102, 0), (650, 50), 25)
+    pygame.draw.circle(screen, (0, 102, 0), (650, 200), 25)
 
     pygame.draw.circle(screen, (0, 102, 0), (400, 345), 25)
     pygame.draw.circle(screen, (0, 102, 0), (400, 495), 25)
@@ -60,13 +60,13 @@ while not done:
     pygame.draw.circle(screen, (0, 102, 0), (650, 345), 25)
     pygame.draw.circle(screen, (0, 102, 0), (650, 495), 25)
 
-    pygame.draw.circle(screen, (0, 102, 0), (400, 625), 25)
-    pygame.draw.circle(screen, (0, 102, 0), (400, 775), 25)
+    pygame.draw.circle(screen, (0, 102, 0), (400, 650), 25)
+    pygame.draw.circle(screen, (0, 102, 0), (400, 800), 25)
 
-    pygame.draw.circle(screen, (0, 102, 0), (650, 625), 25)
-    pygame.draw.circle(screen, (0, 102, 0), (650, 775), 25)
+    pygame.draw.circle(screen, (0, 102, 0), (650, 650), 25)
+    pygame.draw.circle(screen, (0, 102, 0), (650, 800), 25)
 
     pygame.display.flip()
-    clock.tick(60)
+    clock.tick(15)
 
     print(kelnerX,kelnerY)
